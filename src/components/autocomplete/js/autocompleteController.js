573017b4ae27bfa26586e89d94ece80c504ed235
Juan Alvarez <jalvarez@trimstudios.com.ar>
2017-02-14 12:13:08 -0300

    Local modifications to autocomplete item rendering logic

--- src/components/autocomplete/js/autocompleteController.js
+++ src/components/autocomplete/js/autocompleteController.js
@@ -608,6 +608,10 @@ function MdAutocompleteCtrl ($scope, $element, $mdUtil, $mdConstant, $mdTheming,
    * @returns {*}
    */
   function getDisplayValue (item) {
+    var itemText = getItemText(item);
+    return $q.when(itemText === undefined ? item : itemText);
+
+    /*
     return $q.when(getItemText(item) || item).then(function(itemText) {
       if (itemText && !angular.isString(itemText)) {
         $log.warn('md-autocomplete: Could not resolve display value to a string. ' +
@@ -616,6 +620,7 @@ function MdAutocompleteCtrl ($scope, $element, $mdUtil, $mdConstant, $mdTheming,
 
       return itemText;
     });
+    */
 
     /**
      * Getter function to invoke user-defined expression (in the directive)
